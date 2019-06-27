7b8ea0483c11ea036a6aa3a3789a5f56b494a267
Andrew Fei <afei3@illinois.edu>
2019-06-27 01:20:21 -0400

    Created Enemy class + added createBounty function

--- src/src/BountyCreator.java
+++ src/src/BountyCreator.java
@@ -1,2 +1,14 @@
+import java.io.PrintWriter;
+
 public class BountyCreator {
+    public static void createBounty(String fileName) {
+        try {
+            PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+            writer.println("The first line");
+            writer.println("The second linne");
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Get Dabbed on");
+        }
+    }
 }
---
+++ src/src/Enemy.java
@@ -0,0 +1,5 @@
+public class Enemy {
+    private String name;
+    private int[] stats = new int[6]; //STR DEX CON INT WIS CHA
+
+}
